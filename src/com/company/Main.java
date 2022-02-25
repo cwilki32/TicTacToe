a6608c5e0ad61047bb630081ec0a2dea8063b1a4
cwilki32 <collin.wilkins@detroitlabs.com>
2022-02-25 12:41:37 -0500

    Git init

---
+++ src/com/company/Main.java
@@ -0,0 +1,18 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+//        Create a new project in IntelliJ
+//        Create a tic tac toe game
+//        The game should display the “board” grid in the console
+//        The game should allow a user to choose where to put their mark on the board (x’s or o’s)
+//        The game should have the computer place their mark after a player’s turn
+//        The game should check for 3 in a row marks to declare a winner
+//        The game should determine randomly if the player is using x’s or o’s as their mark
+//        Whoever (player or computer) is assigned x’s should play first
+//        The game should ask you to play again once complete
+//        The game should track your wins and loses
+//        The game should detect a tie when the board is full but there is no winner
+    }
+}
